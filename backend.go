f9b1ef13486a12534b0c1389ae4b3b56a84fd14d
Omar Polo <op@omarpolo.com>
2025-10-15 07:37:02 +0000

    typo

--- backend.go
+++ backend.go
@@ -7,7 +7,7 @@ import (
 
 type Backend interface {
 	// List returns an iterator of plugin names,
-	// e.g. s3_v1.0.0_openbsd_amd64.ptar, optinally filtered by
+	// e.g. s3_v1.0.0_openbsd_amd64.ptar, optionally filtered by
 	// the given name, or an error.
 	List(name string) iter.Seq2[*Package, error]
 
