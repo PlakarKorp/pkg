c99bb7f8dbbeeec8bdd419392ed43487b083386c
Eric Faurot <eric@faurot.net>
2026-03-11 11:53:23 +0100

    add connector type

--- var.go
+++ var.go
@@ -6,6 +6,8 @@ type ResourceClass string
 
 type ResourceSubClass string
 
+type ConnectorType string
+
 const (
 	ResourceClassUndefined     ResourceClass = ""
 	ResourceClassUnknown       ResourceClass = "unknown"
@@ -19,6 +21,12 @@ const (
 	ResourceSubClassUnknown   ResourceSubClass = "unknown"
 	ResourceSubClassS3        ResourceSubClass = "s3"
 	ResourceSubClassGCS       ResourceSubClass = "gcs"
+
+	ConnectorTypeImporter       ConnectorType = "importer"
+	ConnectorTypeExporter       ConnectorType = "exporter"
+	ConnectorTypeStorage        ConnectorType = "storage"
+	ConnectorTypeSecretProvider ConnectorType = "secret_provider"
+	ConnectorTypeInventory      ConnectorType = "inventory"
 )
 
 var resourceClasses []ResourceClass = []ResourceClass{
@@ -38,10 +46,22 @@ var resourceSubClasses []ResourceSubClass = []ResourceSubClass{
 	ResourceSubClassGCS,
 }
 
+var connectorTypes []ConnectorType = []ConnectorType{
+	ConnectorTypeExporter,
+	ConnectorTypeImporter,
+	ConnectorTypeStorage,
+	ConnectorTypeSecretProvider,
+	ConnectorTypeInventory,
+}
+
 func (c ResourceClass) IsValid() bool {
 	return slices.Contains(resourceClasses, c)
 }
 
 func (c ResourceSubClass) IsValid() bool {
 	return slices.Contains(resourceSubClasses, c)
+}
+
+func (c ConnectorType) IsValid() bool {
+	return slices.Contains(connectorTypes, c)
 }
